11d9a0d56afb3af824c9be5d92a4ec7033faab3d
dfldylan <dfldylan@qq.com>
2023-02-22 18:50:16 +0800

    add obj_to_bgeo.py

---
+++ datasets/obj_to_bgeo.py
@@ -0,0 +1,20 @@
+import os
+import open3d as o3d
+import numpy as np
+from datasets.create_physics_scenes import obj_surface_to_particles, write_bgeo_from_numpy
+
+def obj_to_bgeo(bb_obj, box_output_path, translation=None, scale=None):
+    # convert bounding box to particles
+    bb, bb_normals = obj_surface_to_particles(bb_obj)
+    bb *= scale
+    bb += translation
+    write_bgeo_from_numpy(box_output_path, bb, bb_normals)
+
+if __name__ == '__main__':
+    # select random bounding box
+    bb_obj = r'/workdir/fluidmlp3/data/test_scenes/01/bgeo/UnitBox.obj'
+    translation = [0, 2, 0]
+    scale = [4.0, 4.0, 1.5]
+    # bounding box
+    box_output_path = os.path.join('/workdir/fluidmlp3/data/test_scenes/01/bgeo/box.bgeo')
+    obj_to_bgeo(bb_obj, box_output_path, translation, scale)
